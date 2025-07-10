b399473e644d34f826dc10a87f3f2da978fedc17
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2025-07-10 18:05:59 +0100

    Add support for cloning graph s into instances

--- pyzx/graph/graph_s.py
+++ pyzx/graph/graph_s.py
@@ -45,8 +45,8 @@ class GraphS(BaseGraph[int,Tuple[int,int]]):
         self._inputs: Tuple[int, ...]                   = tuple()
         self._outputs: Tuple[int, ...]                  = tuple()
 
-    def clone(self) -> 'GraphS':
-        cpy = GraphS()
+    def clone[T: GraphS](self, instance: Optional[T] = None) -> T:
+        cpy = instance or GraphS()
         for v, d in self.graph.items():
             cpy.graph[v] = d.copy()
         cpy._vindex = self._vindex
